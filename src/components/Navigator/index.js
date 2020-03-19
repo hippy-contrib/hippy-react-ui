ab94d7b18d7f30edd66e5bed37e5c13fcf1cf273
kidd xu <kiddxu@tencent.com>
2020-03-19 22:51:56 +0800

    feat(Navigator): add Navigator

---
+++ src/components/Navigator/index.js
@@ -0,0 +1,133 @@
+import React from 'react';
+import { StyleSheet, View } from '@hippy/react';
+import Text from '../Text';
+
+import StatusBarWrapper from './StatusBarWrapper';
+import { NavigatorPropTypes, NavigatorDefaultProps } from './props';
+import { flattenStyle } from '../../utils';
+import Icon from '../Icon';
+import { stopPropagation } from '../../utils/event';
+
+import BlueBack from '../../../assets/ios-back-blue.png';
+
+const navigatorHeight = 65;
+const PADDINGTOP = 20;
+const styles = StyleSheet.create({
+	container: {
+		display: 'flex',
+		flexDirection: 'column',
+		height: navigatorHeight,
+		position: 'relative',
+		backgroundColor: '#ffffff',
+		paddingTop: PADDINGTOP,
+		// backgroundColor: color.whitebackgroundColor,
+	},
+	leftContainer: {
+		position: 'absolute',
+		left: 22,
+		bottom: 0,
+		width: 100,
+		height: navigatorHeight,
+		alignItems: 'flex-start',
+		paddingTop: PADDINGTOP,
+		justifyContent: 'center',
+	},
+	titleContainer: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+		paddingHorizontal: 22,
+	},
+	rightContainer: {
+		position: 'absolute',
+		right: 22,
+		bottom: 0,
+		width: 100,
+		height: navigatorHeight,
+		alignItems: 'flex-end',
+		paddingTop: PADDINGTOP,
+		justifyContent: 'center',
+	},
+})
+
+export class Navigator extends React.Component {
+
+	handleLeftClick (event) {
+		console.log('fuck');
+		this.props.onLeftClick(event);
+		return stopPropagation(event);
+	}
+	renderLeft () {
+		const { back, leftContent } = this.props;
+		const backContent = <Icon size='xs' onClick={event => this.handleLeftClick(event)} source={BlueBack} />
+
+		const body = React.isValidElement(leftContent) ? leftContent : <Text size='s' >{ leftContent }</Text>;
+
+		return (
+			<View style={styles.leftContainer}>
+				{
+					back ? backContent : body
+				}
+			</View>
+		);
+	}
+
+	renderRight () {
+		const { rightContent } = this.props;
+		return (
+			<View style={styles.rightContainer}>
+				{
+					React.isValidElement(rightContent) ? rightContent : <Text size='s' >{ rightContent }</Text>
+				}
+			</View>
+		);
+	}
+
+	renderTitle () {
+		const { title, titleStyle } = this.props;
+		return (
+			<View style={[ styles.titleContainer, flattenStyle(titleStyle)]}>
+				{
+					React.isValidElement(title) ? title : <Text size='md' >{ title }</Text>
+				}
+			</View>
+		);
+	}
+	renderItem (pos, customContent) {
+		const style = [ styles[`${pos}Container`], flattenStyle(this.props[`${pos}Style`] || {}) ];
+		const body = this.props[pos];
+		return (
+			<View style={style}>
+				{
+					customContent ||
+					(!React.isValidElement(body) ?
+						<Text style={this.props[`${pos}TextStyle`] || {}} >{ body }</Text> :
+						body)
+				}
+			</View>
+		)
+	}
+	render () {
+		const { containerStyle, transparent, rightContent } = this.props;
+		return (
+			<StatusBarWrapper>
+				<View style={[ styles.container, transparent ? { backgroundColor: 'transparent' } : {}, flattenStyle(containerStyle)]}>
+					{
+						this.renderTitle()
+					}
+					{
+						this.renderLeft()
+					}
+					{
+						this.renderRight()
+					}
+				</View>
+			</StatusBarWrapper>
+		);
+	}
+}
+
+Navigator.propTypes = NavigatorPropTypes;
+Navigator.defaultProps = NavigatorDefaultProps;
+
+export default Navigator;
---
+++ src/components/Navigator/props.js
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types';
+
+import { StyleProps } from '../../types';
+
+export const NavigatorPropTypes = {
+	containerStyle: StyleProps,
+	titleTextStyle: StyleProps,
+	titleStyle: StyleProps,
+	title: PropTypes.oneOfType([ PropTypes.string, PropTypes.node ]),
+	leftContent: PropTypes.oneOfType([ PropTypes.string, PropTypes.node ]), // 如果是back类型，显示返回箭头
+	rightContent: PropTypes.oneOfType([ PropTypes.string, PropTypes.node ]),
+	onLeftClick: PropTypes.func, // 点击返回按钮触发事件
+	transparent: PropTypes.bool, // 是否透明
+	back: PropTypes.bool, // 是否显示返回，优先级高于left
+}
+
+export const NavigatorDefaultProps = {
+	containerStyle: {},
+	titleStyle: {},
+	titleTextStyle: {},
+	transparent: false,
+	title: '',
+	leftContent: '',
+	rightContent: '',
+	back: false,
+	onLeftClick: () => {}
+}

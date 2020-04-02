0fa404b75d5bed64aa5eb400d0d3f72cbe8474da
jacyhuang <jacyhuang@tencent.com>
2020-04-02 10:48:40 +0800

    feat(List): listview传入参数剔除router参数

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -51,15 +51,13 @@ class List extends React.Component {
 	getStyle () {
 	}
 	render () {
-		let props = {
-			...this.props,
-			renderRow: this.getRenderRow
-		}
+		let {location, match, history, props} = this.props
 		let stylePro = Array.isArray(props.style) ? props.style : [props.style]
 		return (
 			<ListView
 				ref={this.listRef}
 				{...props}
+				renderRow={this.getRenderRow}
 				style={[styles.container, {
 					paddingTop: this.state.offsetTop
 				}, ...stylePro]}
